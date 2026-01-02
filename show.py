0aad918f39adb5535f5faa3812cdcc0c272a3918
riansetyawan <rian.setyawan@bankraya.co.id>
2026-01-02 09:35:59 +0700

    add query sql

--- show.py
+++ show.py
@@ -1,15 +1,32 @@
+# import sys
+# from pathlib import Path
+
+# model = sys.argv[1]
+# sql_path = Path("dbt_ci/models") / f"{model}.sql"
+
+# if not sql_path.exists():
+#     raise FileNotFoundError(sql_path)
+
+# sql = sql_path.read_text()
+
+# if "select" not in sql.lower():
+#     raise ValueError("Invalid SQL")
+
+# print("✅ SQL validation passed")
+
 import sys
 from pathlib import Path
 
-model = sys.argv[1]
-sql_path = Path("dbt_ci/models") / f"{model}.sql"
+if len(sys.argv) < 2:
+    raise ValueError("Ticket ID harus diisi, contoh: python show.py EDM-0001")
 
-if not sql_path.exists():
-    raise FileNotFoundError(sql_path)
+ticket_id = sys.argv[1]
+ticket_path = Path("dbt_ci/models") / ticket_id
+print(ticket_path)
+if not ticket_path.exists():
+    raise FileNotFoundError(f"Ticket {ticket_id} tidak ditemukan")
 
-sql = sql_path.read_text()
+print(f"Validasi ticket: {ticket_id}")
 
-if "select" not in sql.lower():
-    raise ValueError("Invalid SQL")
-
-print("✅ SQL validation passed")
+for sql_file in ticket_path.rglob("*.sql"):
+    print(f"Checking SQL: {sql_file}")
